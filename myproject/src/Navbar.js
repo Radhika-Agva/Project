14ac4e0e362ffc6365c8895d753d385d607b03d1
radhika130801 <radhikaiyengar@ddtech.in>
2022-11-10 12:15:42 +0530

    E-Responsive Navbar

--- myproject/src/Navbar.js
+++ myproject/src/Navbar.js
@@ -1,3 +1,4 @@
+
 import React from 'react'
 import logo from './videos/logo.png'
 
@@ -6,19 +7,18 @@ function Navbar(){
         <div className="navbar">
 
             <img className='logo' src ={logo} alt='Logo'></img>
+            <input type="checkbox" id ="click"/>
+                <label for="click" class="menu-btn">
+                    <i class="fas fa-bars"></i>
+                </label>
             <ul>
-                <li><a href="#" class='active' >Home</a></li>
+                <li><a href="Home" class='active' >Home</a></li>
                 <li><a href="#">About Us</a></li>
                 <li><a href="#">Contact Us</a></li>
                 <li><a href="#">Press</a></li>
             </ul>
-            <input type="checkbox" id ="check"/>
-                <label for="check" class="checkbtn">
-                    <i class="fas fa-bars"></i>
-                </label>
         </div>
     )
 
-
 }
-export default Navbar;
+export default Navbar
